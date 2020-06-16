0a57e66b140529c2b65de9bc3d599aeace359422
Harsh <harsh.vaish@bajajcapital.com>
2020-06-16 17:16:15 +0530

    bcci

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -10,14 +10,8 @@ import { EppAcion } from './services/model/epp-acion';
 export class AppComponent {
   title = 'app';
 
-  public datasource: EppAcion[];
-  private subscriptionResults = new Subscription();
-
-  constructor(private lookupService: LookupService) {
-   this.lookupService.getEppAction().subscribe((data) => {
-     this.datasource = data;
-     console.log(data);
-        });
+  constructor() {
+ 
 
 
   }
