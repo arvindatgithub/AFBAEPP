6375603db90f0888aabbf98428bd19c9b833b2ed
rajinat <rajalakshmir53@gmail.com>
2020-06-16 14:49:30 +0530

    Remaining Products added

--- ClientApp/src/app/basic-group-life/basic-group-life.component.ts
+++ ClientApp/src/app/basic-group-life/basic-group-life.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-basic-group-life',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicGroupLifeComponent implements OnInit {
 
+  @Input() lookupSitusState: any;
+  public minDate = new Date().toISOString().slice(0,10);
   constructor() { }
 
   ngOnInit() {
--- ClientApp/src/app/vol-group-life/vol-group-life.component.ts
+++ ClientApp/src/app/vol-group-life/vol-group-life.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-vol-group-life',
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vol-group-life.component.css']
 })
 export class VolGroupLifeComponent implements OnInit {
-
+  
+  @Input() lookupSitusState: any;
+  public minDate = new Date().toISOString().slice(0,10);
   constructor() { }
 
   ngOnInit() {
