f51ee50c2e176076c39f186b922fd46d8c8180c9
rajinat <rajalakshmir53@gmail.com>
2020-06-21 01:15:54 +0530

    Custom bulk update Api changes

--- ClientApp/src/app/custom-bulk-template/custom-bulk-template.component.ts
+++ ClientApp/src/app/custom-bulk-template/custom-bulk-template.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { CustomeppattributeService } from '../services/customeppattribute.service';
 import * as XLSX from 'xlsx';
 import { ExcelService } from '../services/excel.service';
@@ -12,35 +12,65 @@ import { ExcelService } from '../services/excel.service';
 export class CustomBulkTemplateComponent implements OnInit {
 
   productsList: any;
-  selectedProduct = '';
-  grpNum = '';
-  availableFields : any;
-  selectedFields : any;
-  FPPGselected = [];
 
-  constructor( private customattributeService: CustomeppattributeService, private excelService: ExcelService) {
-   
+  selectedProduct: any;
+  grpNum: any;
+
+  availableFields: any;
+  selectedFields: any;
+
+  fieldsByGrpPrdt: any;
+  fieldsByPrdt: any;
+  editFlag:boolean;
+  userData = {
+    groupNumber: '',
+    product: '',
+  };
+  submitted = false;
+
+
+  constructor(private customattributeService: CustomeppattributeService, private excelService: ExcelService) {
+
   }
 
   ngOnInit() {
-    
     this.customattributeService.getEppProducts().subscribe((data) => {
       this.productsList = data;
     });
+  }
+
+  onSubmit(form, formData) {
+    this.grpNum = formData.groupNumber;
+    this.selectedProduct = formData.product;
+    this.getFields(this.grpNum, this.selectedProduct);
+  }
+
+  getFields(grpNbr, productNm) {
+
+    this.customattributeService.getProductFieldsByGroup(grpNbr, productNm).subscribe(
+      data => {
+        console.log('data by group ' + JSON.stringify(data));
+        this.fieldsByGrpPrdt = data;
+        this.availableFields = this.fieldsByGrpPrdt.availableList;
+        this.selectedFields = this.fieldsByGrpPrdt.selectedList;
+        this.editFlag = true;
+      }, err => {
+        console.log("error occurred " + err.status);
+        this.editFlag = false;
+        this.customattributeService.getProductFields(productNm).subscribe((data) => {
+          console.log('data by products' + JSON.stringify(data));
+          this.fieldsByPrdt = data;
+          this.availableFields = this.fieldsByPrdt.availableList;
+          this.selectedFields = this.fieldsByPrdt.selectedList;
+        });
+      }
+
+    );
 
   }
-  getProductTemplate(product){
-    console.log('value '+ product +'selected product'+ this.selectedProduct);
-     this.customattributeService.getProductFields(product).subscribe((data) => {
-       let dataLists: any = data;
-      this.availableFields = dataLists.availableList;
-      this.selectedFields = dataLists.selectedList;
-    });
-  
-  }
-  
-  updateCheckedOptions(option, event){
-    console.log('option', JSON.stringify(option) + " event " + JSON.stringify(event) );
+
+  updateCheckedOptions(option, event) {
+    console.log('option', JSON.stringify(option) + " event " + JSON.stringify(event));
     console.log(this.selectedFields);
   }
 
@@ -49,9 +79,9 @@ export class CustomBulkTemplateComponent implements OnInit {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
       transferArrayItem(event.previousContainer.data,
-                        event.container.data,
-                        event.previousIndex,
-                        event.currentIndex);
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
     }
 
     console.log(this.selectedFields);
@@ -63,14 +93,26 @@ export class CustomBulkTemplateComponent implements OnInit {
   }
 
   saveLayout() {
-    let reqObj= {
-      groupNum: this.grpNum,
+    for(let i=0; i<this.selectedFields.length; i++){
+      this.selectedFields[i].clmnOrdr = i+1;
+    }
+    let reqObj = {
+      grpNbr: this.grpNum,
       productNm: this.selectedProduct,
-      selectedList: this.selectedFields
+      eppPrdAttrFields: this.selectedFields,
+      isEdit: this.editFlag
     };
-    console.log(' request obj '+JSON.stringify(reqObj));
-
+    if (this.editFlag) {
+      reqObj.isEdit = true;
+      this.customattributeService.editProdAttr(reqObj).subscribe((data) => {
+        console.log('edit request response' + data);
+      });
+    } else {
+      console.log('request obj' + JSON.stringify(reqObj));
+      this.customattributeService.addProdAttr(reqObj).subscribe((data) => {
+        console.log('Add request response' + data);
+      });
+    }
   }
-
 
 }
--- ClientApp/src/app/services/customeppattribute.service.ts
+++ ClientApp/src/app/services/customeppattribute.service.ts
@@ -18,7 +18,21 @@ export class CustomeppattributeService {
   getProductFields(product) {
     const GroupsDataUrl = environment.apiurl + 'Custom/product/' + product;
     return this.http.get(GroupsDataUrl);
+  }
+
+  getProductFieldsByGroup(grpNbr,productNm) {
+    const GroupsDataUrl = environment.apiurl + 'Custom/grpNbr/' + grpNbr + '/productNm/' + productNm;
+    return this.http.get(GroupsDataUrl);
+  }
+
+  addProdAttr(requestObj){
+    const GroupsDataUrl = environment.apiurl + 'Custom/EppAddPrdctAttrbt';
+    return this.http.post(GroupsDataUrl,requestObj);
+  }
   
+  editProdAttr(requestObj){
+    const GroupsDataUrl = environment.apiurl + 'Custom/EppEditPrdctAttrbt';
+    return this.http.put(GroupsDataUrl,requestObj);
   }
 
 }
