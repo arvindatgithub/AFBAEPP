0335853bbc893ff0e9fcf5354ad66667841503ac
rajinat <rajalakshmir53@gmail.com>
2020-06-23 00:10:48 +0530

    EPCI BGL radio

--- ClientApp/src/app/basic-group-life/basic-group-life.component.ts
+++ ClientApp/src/app/basic-group-life/basic-group-life.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { LookupService } from '../services/lookup.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-basic-group-life',
   templateUrl: './basic-group-life.component.html',
   styleUrls: ['./basic-group-life.component.css']
 })
 export class BasicGroupLifeComponent implements OnInit {
-
+  basicGrpLfformgrp: FormGroup;
   @Input() lookupValue: any;
   @Input() dateValue: any;
   situsValue:string;
@@ -19,7 +20,7 @@ export class BasicGroupLifeComponent implements OnInit {
   disabled = false;
   public minDate = new Date().toISOString().slice(0,10);
 
-  constructor(private lookupService: LookupService) {
+  constructor(private lookupService: LookupService, private fb:FormBuilder) {
   }
 
   ngOnInit() {
@@ -29,9 +30,24 @@ export class BasicGroupLifeComponent implements OnInit {
         console.log("data", data);
         this.lookUpDataSitusStates = data.situsState;
       });
+
+      this.basicGrpLfformgrp = this.fb.group({
+        FCbasicEffectiveDate_Action: ["",Validators.required],
+        FCbasicEffectiveDate: [this.lookupValue,Validators.required],
+        FCbasicSitusState_Action: ["",Validators.required],
+        FCbasicSitusState: ["",Validators.required],
+        FCbasicEmpFcAmt_Action: ["",Validators.required],
+        FCbasicEmpFcAmt: ["",Validators.required],
+        FCbasicADDRider_Action: ["",Validators.required],
+        FCbasicADDRider: ["",Validators.required],
+      })
+
   }
   // getLookupValueSitusState(value: any){
   //   this.lookupSitusStateValue = value;
   // }
+  onItemChange(value){
+    console.log(" Value is : ", value );
+ }
 
 }
--- ClientApp/src/app/employer-paid-ci/employer-paid-ci.component.ts
+++ ClientApp/src/app/employer-paid-ci/employer-paid-ci.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { LookupService } from '../services/lookup.service';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-employer-paid-ci',
   templateUrl: './employer-paid-ci.component.html',
   styleUrls: ['./employer-paid-ci.component.css']
 })
 export class EmployerPaidCIComponent implements OnInit {
+  empCIformgrp: FormGroup;
   @Input() lookupValue: any;
   @Input() dateValue: any;
   situsValue:string;
@@ -18,7 +20,7 @@ export class EmployerPaidCIComponent implements OnInit {
   disabled = false;
   public minDate = new Date().toISOString().slice(0,10);
 
-  constructor(private lookupService: LookupService) { }
+  constructor(private lookupService: LookupService, private fb:FormBuilder) { }
 
   ngOnInit() {
     this.lookupService.getLookupsData()
@@ -27,9 +29,26 @@ export class EmployerPaidCIComponent implements OnInit {
       console.log("data", data);
       this.lookUpDataSitusStates = data.situsState;
     });
+
+    this.empCIformgrp = this.fb.group({
+      FCempCIEffectiveDate: ["",Validators.required],
+      FCempCIEffectiveDate_Action: ["",Validators.required],
+      FCempCISitusState_Action: ["",Validators.required],
+      FCempCISitusState: ["",Validators.required],
+      FCempCISpouseFcAmt: ["",Validators.required],
+      FCempCIEmpFcAmt: ["",Validators.required],
+      FCempCIEmpFcAmt_Action: ["",Validators.required],
+      FCempCIPlanCode: ["",Validators.required],
+      FCempCIPlanCode_Action: ["",Validators.required],
+      FCempCIChdFcAmt: ["",Validators.required],
+      FCempCIChdFcAmt_Action: ["",Validators.required],
+    })
   }
   // getLookupValueSitusState(value: any){
   //   this.lookupSitusStateValue = value;
   // }
 
+  onItemChange(value){
+    console.log(" Value is : ", value );
+ }
 }
