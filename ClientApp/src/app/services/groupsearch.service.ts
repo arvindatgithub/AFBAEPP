8aa7a10fdaa8257f4d85ec933803e2768fe522fe
rajinat <rajalakshmir53@gmail.com>
2020-06-27 12:28:13 +0530

    Group data service added on load of group setup

--- ClientApp/src/app/services/groupsearch.service.ts
+++ ClientApp/src/app/services/groupsearch.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from './../../environments/environment'
 import { HttpClient } from '@angular/common/http';
+import { Observable, Subject, BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +9,18 @@ import { HttpClient } from '@angular/common/http';
 export class GroupsearchService {
 
   constructor(private http: HttpClient) { }
+  private groupNumber = new BehaviorSubject<string>('');
+  castGroupNumber = this.groupNumber.asObservable();
 
   getGroupsData() {
     const GroupsDataUrl = environment.apiurl + 'GroupSetup/GetGroupsData';
     return this.http.get(GroupsDataUrl);
   }
+
+  
+
+  existingGrpNbrSelected(value){
+    this.groupNumber.next(value);
+ }
   
 }
