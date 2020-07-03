15d032bcb889b1a6ec9297026c51f33d98046896
Yash Vaish <yasvaish@in.ibm.com>
2020-07-03 11:24:02 +0530

    testing accident

--- ClientApp/src/app/accident/accident.component.spec.ts
+++ ClientApp/src/app/accident/accident.component.spec.ts
@@ -1,25 +1,63 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 
 import { AccidentComponent } from './accident.component';
 
-describe('AccidentComponent', () => {
-  let component: AccidentComponent;
-  let fixture: ComponentFixture<AccidentComponent>;
+import { ReactiveFormsModule } from '@angular/forms';
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AccidentComponent ]
-    })
-    .compileComponents();
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
+import { HTTP_INTERCEPTORS, HttpClient, HttpErrorResponse } from '@angular/common/http';
+
+import { GroupsearchService } from '../services/groupsearch.service';
+import { DatePipe } from '@angular/common';
+
+import { EppCreateGrpSetupService } from '../services/epp-create-grp-setup.service';
+
+fdescribe('AccidentComponent', () => {
+
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule,ReactiveFormsModule], 
+    declarations:[AccidentComponent],
+    providers: [GroupsearchService,EppCreateGrpSetupService]
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AccidentComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+   it('should be created', () => {
+    const service: GroupsearchService = TestBed.get(GroupsearchService);
+    expect(service).toBeTruthy();
+   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+   it('should have getData function', () => {
+    const service: GroupsearchService = TestBed.get(GroupsearchService);
+    expect(service.getGroupsData).toBeTruthy();
+   });
+
+   it('should be created', () => {
+    const service: EppCreateGrpSetupService = TestBed.get(EppCreateGrpSetupService);
+    expect(service).toBeTruthy();
+   });
+
+   it('should have getData function', () => {
+    const service: EppCreateGrpSetupService = TestBed.get(EppCreateGrpSetupService);
+    expect(service.getGroupNbrEppData).toBeTruthy();
+   });
+//////
+
+  it('should be created:AccidentComponent', () => {
+    const service: AccidentComponent = TestBed.get(GroupsearchService);
+    expect(service).toBeTruthy();
+   });
+
+   it('should be created:EppCreateGrpSetupService', () => {
+    const service: EppCreateGrpSetupService = TestBed.get(GroupsearchService);
+    expect(service).toBeTruthy();
+   });
+
+  // it('should have getData function:AccidentComponent', () => {
+  //   const service: AccidentComponent = TestBed.get(GroupsearchService);
+  //   expect(service.myForm).toBeTruthy();
+  //  });
+
+
+
 });
+// ng test --code-coverage
--- ClientApp/src/app/group-setup/group-setup.component.spec.ts
+++ ClientApp/src/app/group-setup/group-setup.component.spec.ts
@@ -1,25 +1,52 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { GroupSetupComponent } from './group-setup.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { DebugElement } from '@angular/core';
 
 describe('GroupSetupComponent', () => {
   let component: GroupSetupComponent;
   let fixture: ComponentFixture<GroupSetupComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
+  const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GroupSetupComponent ]
+      imports: [ReactiveFormsModule, FormsModule, HttpClientTestingModule],
+      declarations: [ GroupSetupComponent ],
+      providers: [
+        { provide: Router, useValue: routerSpy },
+      ]
     })
-    .compileComponents();
-  }));
+    .compileComponents().then(() => {
+      fixture = TestBed.createComponent(GroupSetupComponent);
+      component = fixture.componentInstance;
+      component.ngOnInit();
+      fixture.detectChanges();
+      de = fixture.debugElement.query(By.css('form'));
+      el = de.nativeElement;
+    });
+  }));;
+ 
 
   beforeEach(() => {
     fixture = TestBed.createComponent(GroupSetupComponent);
     component = fixture.componentInstance;
+    component.ngOnInit();
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('form invalid when empty', () => {
+    component.groupSetupFG.controls.fcEffDate.setValue('');
+    // component.registerForm.controls.email.setValue('');
+    // component.registerForm.controls.phone.setValue('');
+    // component.registerForm.controls.password.setValue('');
+    // component.registerForm.controls.confirmPassword.setValue('');
+    expect(component.groupSetupFG.valid).toBeFalsy();
   });
+ 
+
 });
