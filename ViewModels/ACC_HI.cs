0de8c71c9220988d0ef15c06a717cb25b964f02c
Arvind <arvind.a.pal@capgemini.com>
2020-07-03 15:53:21 +0530

    added changes request by Kendra

--- ViewModels/ACC_HI.cs
+++ ViewModels/ACC_HI.cs
@@ -10,12 +10,9 @@ namespace AFBA.EPP.ViewModels
    
         public string effctv_dt { get; set; }
         public string grp_situs_state { get; set; }
-        public string rate_lvl { get; set; }
-
         public string effctv_dt_action { get; set; }
         public string grp_situs_state_action { get; set; }
-        public string rate_lvl_action { get; set; }
-
+      
         public string owner_smkr_no_smkr_action { get; set; }
         public string sp_smkr_no_smkr_action{ get; set; }
 
@@ -31,6 +28,8 @@ namespace AFBA.EPP.ViewModels
         public string sp_gndr { get; set; }
 
 
+        public string rate_lvl { get; set; }
+        public string rate_lvl_action { get; set; }
         public string ch_fname_01_action { get; set; }
         public string ch_dob_01_action { get; set; }
         public string ch_gndr_01_action { get; set; }
--- ViewModels/HI.cs
+++ ViewModels/HI.cs
@@ -11,5 +11,20 @@ namespace AFBA.EPP.ViewModels
         public string grp_situs_state { get; set; }
          public string effctv_dt_action { get; set; }
         public string grp_situs_state_action { get; set; }
+
+        public string sp_fname { get; set; }
+        public string sp_dob { get; set; }
+        public string sp_gndr { get; set; }
+
+
+        public string rate_lvl { get; set; }
+        public string rate_lvl_action { get; set; }
+        public string ch_fname_01_action { get; set; }
+        public string ch_dob_01_action { get; set; }
+        public string ch_gndr_01_action { get; set; }
+
+        public string sp_fname_action { get; set; }
+        public string sp_dob_action { get; set; }
+        public string sp_gndr_action { get; set; }
     }
 }
