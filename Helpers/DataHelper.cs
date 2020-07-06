1090bf2af8eab536eb54462a748d167727037914
Arvind <arvind.a.pal@capgemini.com>
2020-07-06 09:10:47 +0530

    Productcode issue fixed

--- Helpers/DataHelper.cs
+++ Helpers/DataHelper.cs
@@ -29,7 +29,7 @@ namespace AFBA.EPP.Helpers
             }
             else
             {
-                planCodeViewModel.ProductId = result.ProductId;
+                planCodeViewModel.ProdctCdId = result.ProdctCdId;
             }
             return planCodeViewModel;
          }
